7bdad859dc8515730847ac622e038a3738a35fe4
kuttyhub0 <87655495+kuttyhub0@users.noreply.github.com>
2021-08-30 03:17:07 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
+        InlineKeyboardButton('Developers', url='https://t.me/TMS_Linkz'),
         InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/kuttyhub')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
